d2ab594fce81e3737d56d22d725d7e7ed80dfd4d
naleeyyy <kahmeti123@gmail.com>
2023-02-01 20:41:52 +0100

    readme file

--- main.js
+++ main.js
@@ -5,10 +5,10 @@ const outputDiv     = document.getElementById('output')
 const m = math.create(math.all)
 
 drawBtn.addEventListener('click', () => {
-  let equation = equationInput.value // TASK check for undefined
+  let equation = equationInput.value // TASK #1
   
   let scope = {
-    x: 1, // TASK input x here somehow (1 is hardcoded value for test)
+    x: 1, // TASK #2 (1 is hardcoded value for test)
   }
 
   let result = m.evaluate(equation, scope)
--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
-##Refrences:
-    -   https://mathjs.org/docs/index.html
+## Refrences
+    1. MathJS Docs   
+        https://mathjs.org/docs/index.html
+
+## Tasks
+                             filename   : line
+    -------------------------------------------
+    1. Check for undefined  (main.js    : 8   )
+    2. Input x here somehow (main.js    : 11  )
+    3. Add styles           (styles.css : 5   )
