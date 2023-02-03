fd94f2a3063dc6ac3822ece9c524bbc74525c509
naleeyyy <kahmeti123@gmail.com>
2023-02-03 12:09:18 +0100

    better graph

--- main.js
+++ main.js
@@ -1,69 +1,78 @@
+// Constant Variabless
+const POINT_SIZE = 3
+const STEP_SIZE  = 0.005
+const FACTOR     = 200;
+
+// HTML elements
 const equationInput = document.getElementById('equation')
 const drawBtn       = document.getElementById('drawBtn')
 const outputDiv     = document.getElementById('output')
 const canvas        = document.getElementById('canvas')
 
-const ctx = canvas.getContext('2d');
-const m = math.create(math.all)
+// Setup variables
+const ctx = canvas.getContext('2d')
+const m   = math.create(math.all)
+let minX, minY, maxX, maxY, clientWidth, clientHeight
+let points = []
 
-const clientWidth  = window.innerWidth;
-const clientHeight = window.innerHeight;
+const calculateCanvasSize = () => {
+  clientWidth  = window.innerWidth;
+  clientHeight = window.innerHeight;
 
-canvas.width  = clientWidth;
-canvas.height = clientHeight;
+  canvas.width  = clientWidth;
+  canvas.height = clientHeight;
 
-let min_X=-20;
-let max_X=20;
-let min_Y=-5;
-let max_Y=5;
+  minX = -(clientWidth/2);
+  maxX =  clientWidth/2;
+  minY = -(clientHeight/2);
+  maxY =  clientHeight/2;
+}
 
-let axis_Zero;
+const drawAxis = (ctx) => {
+  // Draw X axis
+  ctx.beginPath();
+  ctx.moveTo(0, clientHeight/2);
+  ctx.lineTo(clientWidth, clientHeight/2);
+  ctx.stroke();
 
-// X axis
-ctx.beginPath();
-ctx.moveTo(0,clientHeight/2);
-ctx.lineTo(clientWidth,clientHeight/2 );
-ctx.stroke();
-// Y axis
-ctx.beginPath();
-ctx.moveTo(clientWidth/2,0);
-ctx.lineTo(clientWidth/2,clientHeight );
-ctx.stroke();
+  // Draw Y axis
+  ctx.beginPath();
+  ctx.moveTo(clientWidth/2, 0);
+  ctx.lineTo(clientWidth/2, clientHeight);
+  ctx.stroke();
+}
 
-let steps= 0.0003;
+const showPoints = () => {
+  for (const p of points) {
+    ctx.fillRect(p.x + clientWidth/2 - POINT_SIZE, -p.y + clientHeight/2 - POINT_SIZE, POINT_SIZE, POINT_SIZE);  
+  }
+}
 
-let graphPoints=[];
+const resizeCanvas = () => {
+  calculateCanvasSize()
+  drawAxis(ctx)
+  if (points.length > 0) showPoints(points)
+}
+
+resizeCanvas()
+window.addEventListener("resize", () => {
+  resizeCanvas()
+})
 
 drawBtn.addEventListener('click', () => {
   let equation = equationInput.value 
 
-  for(let i = min_X-1 ; i<= max_X-1; i+=1)
+  for (let x = minX-1 ; x <= maxX-1; x+=STEP_SIZE)
   {
-    
-    if(!equation || !equation.includes("x")  ){
-
-      console.log("value is not valid");
-
-      return;
-    }
+    if(!equation || !equation.includes("x")) return
     
     let scope = {
-      x: i, 
+      x: x, 
     }
 
-    let result_Y = m.evaluate(equation, scope)
-    graphPoints.push({i,result_Y});
-    
+    let y = m.evaluate(equation, scope)
+
+    points.push({x: x*FACTOR, y: y*FACTOR});
   }
   showPoints();
 })
-
-function showPoints()
-{
-
-  for (const v of graphPoints) {
-    console.log(v.i + ", " + v.result_Y);
-    ctx.fillRect(v.i +clientWidth/2 ,-v.result_Y + clientHeight/2 ,2,2);  
-  }
-
-}
--- readme.md
+++ readme.md
@@ -5,8 +5,6 @@
 ## Tasks
                              filename   : line
     -------------------------------------------
-    1. Input x here somehow (main.js    : 11  )
-    2. Add styles           (styles.css : 5   )
-    3. draw the graph 
-    4.fuck some hoes !important 
+    1. Fuck some hoes !important 
+        - Kismet de bohet edhe ajo 
 
