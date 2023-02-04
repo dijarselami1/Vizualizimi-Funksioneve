1543cf42df06bf908e7cc7b6ff4b77c10a729696
Dijar <dijarselami40@gmail.com>
2023-02-04 22:20:41 +0100

    qaq per sot

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ const canvas        = document.getElementById('canvas')
 const ctx = canvas.getContext('2d');
 const m = math.create(math.all)
 
-const clientWidth  = window.innerWidth;
-const clientHeight = window.innerHeight;
+const clientWidth   = window.innerWidth;
+const clientHeight  = window.innerHeight;
 
-canvas.width  = clientWidth;
-canvas.height = clientHeight;
+canvas.width     = clientWidth;
+canvas.height    = clientHeight;
 
+let graphColors  = ["#000000", "#8D0808", "#03641A", "#00225B", "#5C00B3", "#FF7575", "#1ED1BE",]
 
 let min_X=-10;
 let max_X=10;
@@ -40,8 +41,6 @@ ctx.stroke();
 // points in x axis
 for(let i = 0; i <= clientWidth; i += axisStep_X ){
 
-  console.log(i + " client widht / 2 " + clientWidth/2)
-
   if( Math.round(i) == Math.round(clientWidth/2)){
 
     continue;
@@ -65,11 +64,8 @@ ctx.fillRect(clientWidth/2 -4 , i-1 , 8,2 );
 
 }
 
-console.log("clientwidht: " + clientWidth + " axis step: " + axisStep_X); 
 
-
-
-let steps= 0.0003;
+let steps= 0.003;
 
 let graphPoints=[];
 
@@ -90,7 +86,9 @@ drawBtn.addEventListener('click', () => {
   
   for(let i = min_X ; i<= max_X; i+=steps)
   {
-    
+    if(equation.includes("log")){
+      i=0;
+    }
     
     
     let scope = {
@@ -103,13 +101,13 @@ drawBtn.addEventListener('click', () => {
     
   }
 
-  showPoints();
+  drawPoints();
 
 
 
 })
 
-function showPoints()
+function drawPoints()
 {
 
   let temp_X = min_X;
@@ -118,18 +116,23 @@ function showPoints()
     x:min_X,
   }
 
-  let equation = equationInput.value;
+  let equation  = equationInput.value;
 
-  let temp_Y = m.evaluate(equation, scope) ;
+  let temp_Y    = m.evaluate(equation, scope) ;
+
+   
 
   for (const v of graphPoints) {
 
-  //  ctx.fillRect(v.i*axisStep_X +clientWidth/2 -1 ,-v.result_Y * axisStep_Y + clientHeight/2 -1 ,2,2);  
+  ctx.lineWidht = 3;
   ctx.beginPath();
   ctx.moveTo((temp_X)*axisStep_X + clientWidth/2 ,-(temp_Y) * axisStep_Y + clientHeight/2 );
   ctx.lineTo(v.i*axisStep_X + clientWidth/2 ,-v.result_Y * axisStep_Y + clientHeight/2 );
+  ctx.strokeStyle = graphColors[3] ;
   ctx.stroke();
   
+  console.log(v.i + "  " + v.result_Y);
+
   temp_X = v.i;
   temp_Y = v.result_Y;
 
